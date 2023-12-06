7028ddd5f2ab20997d74405ee9a8345c58006cef
Herbert Joseph <techniquea2z@gmail.com>
2023-12-06 14:20:23 -0500

    update dependencies

--- config/initializers/api_keys.rb
+++ config/initializers/api_keys.rb
@@ -1,5 +1,5 @@
 ENV['API_URL'] = if Rails.env.development? || Rails.env.test?
-                   Rails.application.credentials.api_urls .development
+                   Rails.application.credentials.api_urls.development
                  elsif Rails.env.staging?
                    Rails.application.credentials.api_url.staging
                  else
