95a2845b1dafb517cd1cb5bf74d9723f084c92f5
Herbert Joseph <techniquea2z@gmail.com>
2023-12-06 14:24:31 -0500

    typo

--- config/initializers/api_keys.rb
+++ config/initializers/api_keys.rb
@@ -1,7 +1,7 @@
 ENV['API_URL'] = if Rails.env.development? || Rails.env.test?
                    Rails.application.credentials.api_urls.development
                  elsif Rails.env.staging?
-                   Rails.application.credentials.api_url.staging
+                   Rails.application.credentials.api_urls.staging
                  else
-                   Rails.application.credentials.api_url.production
+                   Rails.application.credentials.api_urls.production
                  end
