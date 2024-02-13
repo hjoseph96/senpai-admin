4ccad59569cfd2a54c41329782020ec63e9ff32f
dazralsky <dazralsky@gmail.com>
2024-02-13 11:41:05 -0800

    put all routes behind authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for user
+    user_list_path
+  end
+
+  def after_sign_out_path_for user
+    root_path
+  end
+
+  def authenticate_user!
+    redirect_to root_path unless user_signed_in?
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    return redirect_to(user_list_path) if user_signed_in?
   end
 
   def user_list_all
